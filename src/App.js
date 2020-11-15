4dfa7827f02b7f53a1e6ef6e24688aaa6c072e73
unknown <Nelson Oba>
2020-11-15 20:34:46 +0000

    new files

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/BurgerBuilder/Checkout/Checkout';
+import {Route, Switch} from 'react-router-dom';
+import Orders from './containers/BurgerBuilder/Orders/Orders';
 
 class App extends Component {
   render() {
     return (
     <div>
       <Layout>
-        <BurgerBuilder></BurgerBuilder>
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/" component={BurgerBuilder} />
+        </Switch>        
       </Layout>
     </div>
   );
---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import Auxi from '../../../hoc/Auxi/Auxi';
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+        });
+        return (
+            <Auxi>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+            </Auxi>
+        );
+    }
+}
+
+export default OrderSummary;
